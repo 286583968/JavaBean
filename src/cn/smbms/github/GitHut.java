0d3bba5d25936de6fdfd3fa03edd0978dd300993
xxx <286583968@qq.com>
2017-08-03 15:01:14 +0800

    GitHub

---
+++ src/cn/smbms/github/GitHut.java
@@ -0,0 +1,29 @@
+package cn.smbms.github;
+
+public class GitHut {
+	
+	private String userName;
+	private String userCode;
+	private Integer age;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
